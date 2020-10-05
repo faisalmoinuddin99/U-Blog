08d31224240250cf3241cd7bd8f63b939b5417d4
NewUser <newuser@example.com>
2020-10-05 11:01:10 +0530

    checkout 1 completed

--- src/main/java/com/upgrad/ublog/dto/PostDTO.java
+++ src/main/java/com/upgrad/ublog/dto/PostDTO.java
@@ -1,5 +1,7 @@
 package com.upgrad.ublog.dto;
 
+import javax.ejb.Local;
+import java.time.LocalDateTime;
 /**
  * TODO: 2.1. Declare 6 private instance variables in this class named as postId,
  *  emailId, tag, title, description, timestamp. Out of these 6 variables, postId
@@ -13,7 +15,62 @@ package com.upgrad.ublog.dto;
 
 public class PostDTO {
 
-    /*@Override
+    private int postId;
+    private LocalDateTime timestamp;
+
+    private String emailId;
+    private String tag;
+    private String title;
+    private String description;
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+@Override
     public String toString() {
         return "PostDTO{" +
                 "postId=" + postId +
@@ -23,9 +80,9 @@ public class PostDTO {
                 ", description='" + description + '\'' +
                 ", timestamp=" + timestamp +
                 '}';
-    }*/
+    }
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
 		PostDTO postDTO = new PostDTO();
 		postDTO.setPostId(1);
 		postDTO.setEmailId("dummy@dummy.com");
@@ -37,7 +94,7 @@ public class PostDTO {
 		System.out.println(postDTO);
 
 		 //Your output should be similar to as shown below.
-		 //PostDTO{postId=1, emailId='dummy@dummy.com', tag='tag', title='title', description='Description', timestamp=2020-05-22T21:01:34.483363800}
+//		 PostDTO{postId=1, emailId='dummy@dummy.com', tag='tag', title='title', description='Description', timestamp=2020-05-22T21:01:34.483363800}
 
-	}*/
+	}
 }
